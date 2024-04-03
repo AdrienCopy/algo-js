ac82599637ecd7037cf33ececca4111654128f78
Adrien Boels <adrien.boels@gmail.com>
2024-04-03 16:30:22 +0200

    mercredi soir

---
+++ extra-challenges/Change/app.js
@@ -0,0 +1,13 @@
+(() => {
+    // YOUR CODE HERE
+    function computeChange(price, moneyHanded){
+        let change = moneyHanded - price;
+        let tab = [5, 10, 20, 50, 100];
+        }
+        
+
+      let a = computeChange(35, 50);
+      console.log(a);
+
+})();
+
